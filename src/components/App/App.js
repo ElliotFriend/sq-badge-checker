f9d523d09ffa5b6f59b221932879edf231c33850
Elliot J. Voris <elliot@voris.me>
2021-07-27 05:58:32 -0500

    Successfully checking horizon for badge payments
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import Proof from '../Proof/Proof'
-import Product from '../Product/Product'
 
 function App() {
 
--- src/components/Proof/Proof.js
+++ src/components/Proof/Proof.js
@@ -1,34 +1,30 @@
 import React, { useReducer } from 'react';
+import StellarSdk from 'stellar-sdk'
 import albedo from '@albedo-link/intent'
 import {isValidSig} from '../../lib/utils.js'
+import {badgeDetails} from '../../lib/badgeDetails.js'
 
-function loginWithAlbedo() {
-  // e.preventDefault();
-  albedo.publicKey({
-    token: 'WXWO5dYMpIpJCZi6WxPnJ4UILNLtiaW/mXYPaD+Z6mQ='
-  })
-  .then(res => res)
-  console.log("Lasers activated!!!");
-  // console.log(e);
+// const badgeAssetCodes = badgeDetails.reduce((acc, item) => acc.concat(item.code), [])
+// console.log(badgeAssetCodes)
+
+const initialState = {
+  pubkey: '',
+  assets: [{}]
 }
 
-function questerReducer(state, action) {
+function questerReducer(state = initialState, action) {
   switch (action.type) {
     case 'login':
-      // let newState = state.pubkey === '' ? [] : [...state]
-      // newState.pubkey = action.pubkey
-      // newState.signed_message = action.signed_message
-      // newState.signature = action.signature
-      // console.log(newState)
-      console.log(action)
-      return action
+      let newState = {...state}
+      newState.pubkey = action.pubkey
+      return newState
     default:
       return state
   }
 }
 
 export default function Proof() {
-  const [quester, setQuester] = useReducer(questerReducer, [])
+  const [quester, setQuester] = useReducer(questerReducer, initialState)
 
   async function login() {
     let tokenToSign = 'QWxsIGhhaWwgQGthbGVwYWlsIQ=='
@@ -37,17 +33,44 @@ export default function Proof() {
     })
     .then(res => {
       if (isValidSig(res.pubkey, tokenToSign, res.signature)) {
-        console.log("SUCCESS!")
-        setQuester({...res, type: 'login'})
+        setQuester({pubkey: res.pubkey, type: 'login'})
       }
+    })
+  }
+
+  // Function to get the assets on the account that match the SQ code and issuer
+  // This is working, but I'm not sure if it accurately removes any asset that
+  // a user may have purchased...
+  async function getQuestAssets(pubkey) {
+    let server = new StellarSdk.Server('https://horizon.stellar.org')
+    await server.accounts().accountId(pubkey).call()
+    .then(res => {
+      // console.log(res.balances)
+      let r = res.balances
+        .filter((item) => badgeDetails.find(({code, issuer}) => item.asset_code === code && item.asset_issuer === issuer))
+        // .filter(item => badgeAssetCodes.includes(item.asset_code))
+      console.log(r)
+    })
+  }
+
+  async function getQuestPayments(pubkey) {
+    let server = new StellarSdk.Server('https://horizon.stellar.org')
+    await server.payments().forAccount(pubkey).limit(50).call()
+    .then(res => {
+      let r = res.records
+        .filter(item => item.type === 'payment' && item.asset_type !== 'native')
+        .filter(item => badgeDetails.find(({code, issuer}) => item.asset_code === code && item.from === issuer))
+      console.log(r)
     })
   }
 
   return (
     <div className="wrapper">
       <div>Prove yourself as a worthy Quester!</div>
-      <p>Your Public Key: <pre>{quester.pubkey}</pre></p>
+      <p>Your Public Key: <code>{quester.pubkey}</code></p>
       <button type="button" className="btn btn-primary" onClick={login}>Prove It!</button>
+      <button type="button" className="btn btn-success" onClick={() => getQuestPayments(quester.pubkey)}>Get Assets</button>
+
     </div>
   )
 }
