38d8f64a97b3fb4931a946a8623ebf30299cde45
Elliot J. Voris <elliot@voris.me>
2021-07-30 22:39:22 -0500

    Adjusting the way quest series and numbers are calculated here
    mostly, just so that the SSQ01 badge behaves better.
    
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,17 +3,19 @@ import React, { useState } from 'react';
 
 export default function Card(props) {
   const badge = props.badge
+  let badgeSeries = badge.code === "SSQ01" ? "SSQ" : badge.code.substr(2, 2)
+  let badgeQuest = badge.code === "SSQ01" ? "01" : badge.code.substr(4, 2)
   return (
-    <div className="col">
+    <div className="col my-2">
       <div className="card h-100 mx-auto text-dark bg-light border-secondary">
         { !badge.owned ? <div className="not-owned"></div> : null }
-        <img src={"/assets/badges/" + badge.filename} height="128" className="mx-auto nft-badge card-img-top" alt="..." />
+        <img src={"/assets/badges/" + badge.filename} height="128" className="shadow-sm mx-auto nft-badge card-img-top" alt="..." />
         <div className="card-body">
           <h5 className="card-title">{badge.code}</h5>
           <p className="card-text">{badge.description}</p>
         </div>
         <ul className="list-group list-group-flush">
-          <li className="list-group-item"><strong>Series {badge.code.substr(2, 2)}</strong> <span className="text-secondary">Quest {badge.code.substr(4, 2)}</span></li>
+          <li className="list-group-item"><strong>Series {badgeSeries}</strong> <span className="text-secondary">Quest {badgeQuest}</span></li>
           { badge.prize ? <li className="list-group-item">Prize: {badge.prize} XLM</li> : null }
         </ul>
         { badge.link
