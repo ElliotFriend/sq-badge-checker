786ab75d9f089a52229f87b309f9267cb60b8744
Elliot J. Voris <elliot@voris.me>
2021-08-05 21:13:22 -0500

    Changing the display of the link in the footer.
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -129,7 +129,7 @@ function App() {
         </Switch>
         <footer class="footer mt-5 py-3 bg-dark">
           <div class="container">
-            <span class="text-muted">Made with plenty of Adderall by <a target="_blank" href="https://github.com/elliotfriend">ElliotFriend</a></span>
+            <span class="text-muted">Made with plenty of Adderall by <a className="text-decoration-none" target="_blank" referrer="nofollow" href="https://github.com/elliotfriend">ElliotFriend</a></span>
           </div>
         </footer>
       </div>
