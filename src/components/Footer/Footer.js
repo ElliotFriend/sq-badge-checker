5cd47226788ccc40cfe88d275d89b76eade355e5
Elliot J. Voris <elliot@voris.me>
2021-08-06 00:56:11 -0500

    Centering the text in the footer
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
-      <div className="container justify-items-center">
+      <div className="container text-center">
         <span className="text-muted">Made with plenty of Adderall by <a className="text-decoration-none" target="_blank" referrer="nofollow" href="https://github.com/elliotfriend">ElliotFriend</a></span>
       </div>
     </footer>
