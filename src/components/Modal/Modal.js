8505fe4f3ceb435a4eaf67a75d9dfe5b1ceac419
Elliot J. Voris <elliot@voris.me>
2021-08-07 21:41:27 -0500

    Changing some language to make the verification text input a bit clearer.
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -18,7 +18,7 @@ export default function Modal(props) {
           </div>
           <form onSubmit={props.signProofText}>
             <div className="modal-body text-dark">
-              <p>Please enter some verification text below. You will then be asked to sign that text with your secret key using Albedo.</p>
+              <p>Please enter any verification text you'd like below. You will then be asked to sign that text with your secret key using Albedo.</p>
               <input onChange={handleChange} className="form-control" type="text" required placeholder="Perhaps your name, or something..." />
             </div>
             <div className="modal-footer">
