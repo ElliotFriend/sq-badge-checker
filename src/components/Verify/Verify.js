4b7205baf58f8b2adb963befbee1c774e2c23ee7
Elliot J. Voris <elliot@voris.me>
2021-08-08 00:27:34 -0500

    Issue #4: Removing the unused-vars from `Verify.js`
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/Verify/Verify.js
+++ src/components/Verify/Verify.js
@@ -1,31 +1,11 @@
 import './Verify.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom'
-import StellarSdk from 'stellar-sdk'
-import { isValidSig } from '../../lib/utils.js'
 import ProvideToken from './ProvideToken'
 import ProcessToken from './ProcessToken'
 
 export default function Verify() {
   let { basestring } = useParams()
-
-  // if (basestring) { console.log(basestring) } else { console.log("none provided")}
-  // let [verificationToken, setToken] = useState('')
-
-  // useEffect(() => {
-  //   setToken(decodeURIComponent(basestring))
-  // }, [])
-  // console.log(decodeURIComponent(basestring))
-
-
-  // let { hexstring } = useParams()
-  // let verificationString = Buffer.from(hexstring, 'hex').toString()
-  // let verificationObject = JSON.parse(verificationString)
-  // let pubkey = verificationObject.p
-  // let verText = verificationObject.t
-  // let messSig = verificationObject.s
-  // let userAssets = verificationObject.a
-  // let server = new StellarSdk.Server('https://horizon.stellar.org')
 
   return (
     <div className="container">
