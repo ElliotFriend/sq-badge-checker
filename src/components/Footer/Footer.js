dd46af16541ef4b912ac6fef280d90b41d6f49e8
Elliot J. Voris <elliot@voris.me>
2021-08-08 00:45:22 -0500

    Issue #4: Changing the link attributes in the footer
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="footer mt-auto py-3 bg-dark">
       <div className="container text-center">
-        <span className="text-muted">Made with plenty of Adderall by <a className="text-decoration-none" target="_blank" referrer="nofollow" href="https://github.com/elliotfriend">ElliotFriend</a></span>
+        <span className="text-muted">Made with plenty of Adderall by <a className="text-decoration-none" target="_blank" rel="noreferrer" href="https://github.com/elliotfriend">ElliotFriend</a></span>
       </div>
     </footer>
   )
