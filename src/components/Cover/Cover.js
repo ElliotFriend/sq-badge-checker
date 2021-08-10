2fd5858300163d23ee99b12ed4551c44049e43f4
Elliot J. Voris <elliot@voris.me>
2021-08-10 09:39:08 -0500

    Fixing the alignment of the buttons on the Cover component
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -38,7 +38,7 @@ export default function Cover(props) {
           <div className="col-lg-6 mx-auto">
             <p className="fs-5 mb-4">Prove yourself as the worthy Quester you truly are! Check out your badges, see what you're missing, and export a verifiable image to show off to everyone you meet! To get started, simply...</p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center mb-4">
-              <button type="button" className="btn btn-outline-primary btn-lg px-4 me-sm-3 fw-bold" onClick={login}>Connect Albedo</button>
+              <button type="button" className="btn btn-outline-primary btn-lg px-4 fw-bold" onClick={login}>Connect Albedo</button>
             </div>
             <p className="fs-5 mb-4">If you're here to verify a proof, please click the button below to get started.</p>
             <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
