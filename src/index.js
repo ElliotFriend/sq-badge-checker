c9def1269ca133eb08e4559a03eabece8d140906
Elliot J. Voris <elliot@voris.me>
2022-04-19 10:45:53 -0500

    Adding senty to the mix. I guess that could help?
    Signed-off-by: Elliot J. Voris <elliot@voris.me>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
 import './index.css';
 import App from './components/App/App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://85739d886a534ad2b90717858e090e7b@o817895.ingest.sentry.io/6347031",
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
 
 ReactDOM.render(
   <React.StrictMode>
