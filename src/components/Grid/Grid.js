1a60302f07ac85c8214901f18bf6c5054c54aadf
Elliot Voris <elliot@voris.me>
2023-01-06 12:12:42 -0600

    Including the new badges at the top of the "prove" page
    Signed-off-by: Elliot Voris <elliot@voris.me>

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -24,6 +24,11 @@ export default function Grid(props) {
             badgesArr.push(<div key="sideQuestsRow" className="row mt-4"><h2 key="sideQuests">Standalone Side Quest Badges</h2></div>)
             badgesArr.push(<div key="sideQuestsDescription" className="row"><p key="sideDescription"><em>Side Quests</em> are event-based, single-issue badges that prove you've participated in something special within the Stellar Ecosystem.</p></div>)
           }
+        } else if (series === 'SQ05') {
+          badgesArr.push(<div key="sorobanQuestsRow" className="row mt-4"><h2 key="sorobanQuests">Soroban Quest</h2></div>)
+          badgesArr.push(<div key="sorobanQuestsDescription" className="row"><p key="sorobanDescription"><em>Soroban Quest</em> is the bleeding-edge educational experience to prepare developers to use Stellar's upcoming smart contract platform: Soroban! You know, like the abacus. <a target="_blank" rel="nofollow" href="https://soroban.stellar.org">Learn more here!</a></p></div>)
+          badgesArr.push(<div key={`header${i}Row`} className="row mt-3"><h3 key={`header${i}`}>Series {badge.code.substr(2,2)} Badges</h3></div>)
+          badgesArr.push(<div kye="sorobanSeriesFiveDescription" className="row"><p key="seriesFiveDescription">I know, I know... We're starting with <code>Series 05</code>?? Yeah, it's complicated... We're picking up the numbering from where the "legacy quests" ended, but focusing on Soroban.</p></div>)
         } else if (series === 'SQL00') {
           badgesArr.push(<div key="learnQuestsRow" className="row mt-4"><h2 key="learnQuests">Stellar Quest Learn</h2></div>)
           badgesArr.push(<div key="learnQuestsDescription" className="row"><p key="learnDescription"><em>Stellar Quest Learn</em> is the re-imagining of what gamified blockchain education can look like! These badges showcase the guided path through the fundamentals of the Stellar network.</p></div>)
@@ -42,7 +47,7 @@ export default function Grid(props) {
           badgesArr.push(<div key="learnSideQuestsRow" className="row mt-3"><h3 key="learnSideQuests">SQ Learn Side Quests</h3></div>)
           badgesArr.push(<div key="learnSideQuestsDescription" className="row"><p key="sideQuestsDescription">These are more intense, single-topic, deeper dives into interesting, experimental, and/or less-discussed corners of the Stellar network.</p></div>)
         } else {
-          if ((lastSeries === null || /^S?SQL/.test(lastSeries)) && /^SQ0\d$/.test(series)) {
+          if ((lastSeries === null || /^S?SQL/.test(lastSeries)) && /^SQ0[1..4]$/.test(series)) {
             // Put a new header to specify that we're shifting to the legacy badges
             badgesArr.push(<div key="legacyQuestsRow" className="row mt-4"><h2 key="legacyQuests">Stellar Quest Legacy</h2></div>)
             badgesArr.push(<div key="legacyQuestsDescription" className="row"><p key="legacyDescription"><em>Stellar Quest Legacy</em> is the old standard of what we have built our current efforts upon. Series-based, increasingly complex, sometimes-live challenges and puzzles.</p></div>)
